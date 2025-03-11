437e953c87f0bcaf3af63117ace0fd5e6efe20fe
AngryL1on <vadim.angryl1on@gmail.com>
2025-03-11 10:12:39 +0300

    Add feature modules for implementation of the functionality of the application

---
+++ feature/ui-appointment/src/main/kotlin/dev/angryl1on/appointment/presentation/screens/AppointmentScreen.kt
@@ -0,0 +1,16 @@
+package dev.angryl1on.appointment.presentation.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun AppointmentScreen() {
+    Text(text = "It's Main Screen")
+}
+
+@Composable
+@Preview
+fun AppointmentScreenPreview() {
+    AppointmentScreen()
+}
---
+++ feature/ui-main/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    alias(libs.plugins.angryl1on.vetclinic.android.library)
+    alias(libs.plugins.angryl1on.vetclinic.android.library.compose)
+    alias(libs.plugins.angryl1on.vetclinic.android.koin)
+}
+
+android {
+    namespace = "dev.angryl1on.main"
+
+}
+
+dependencies {
+
+    /**
+     * Core dependencies
+     */
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.kotlinx.datetime)
+
+    /**
+     *  Module dependencies
+     */
+    implementation(project(":core:ui"))
+    implementation(project(":core:domain"))
+    implementation(project(":core:model"))
+    implementation(project(":core:common"))
+}
