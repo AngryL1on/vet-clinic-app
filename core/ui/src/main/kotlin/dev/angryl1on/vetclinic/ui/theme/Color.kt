6de53a5229355e969aafeb4aa2a1db1e33ac84bb
AngryL1on <vadim.angryl1on@gmail.com>
2025-03-15 02:04:57 +0300

    Move the theme folder from the app module to the core:ui module. Add colors for project. Add the file with the size of the dimensions. Add typographic settings

---
+++ core/ui/src/main/kotlin/dev/angryl1on/vetclinic/ui/theme/Color.kt
@@ -0,0 +1,25 @@
+package dev.angryl1on.vetclinic.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Blue = Color(0xFF03A9F4)
+val Red = Color(0xFFD32F2F)
+val Black = Color(0xFF424242)
+val White = Color(0xFFFFFFFF)
+
+val ActiveButton = Color(0xFF039BE5)
+val Inputs = Color(0xFFE1F5FE)
+val InputsText = Color(0xFF545454)
+val Cards = Color(0xFFE3F2FD)
+val NavActive = Color(0xFFB3E5FC)
+val NavFill = Color(0xFFECEFF1)
+val Picker = Color(0xFF0277BD)
+val UnactiveButtonAndProgress = Color(0xFFB3E5FC)
---
+++ core/ui/src/main/kotlin/dev/angryl1on/vetclinic/ui/theme/Type.kt
@@ -0,0 +1,106 @@
+package dev.angryl1on.vetclinic.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import ru.angryl1on.vetclinic.ui.R
+
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    )
+)
+
+val VetclinicAppWorkSansFamily = FontFamily(
+    Font(R.font.worksans_medium, FontWeight.Medium),
+    Font(R.font.worksans_regular, FontWeight.Normal)
+)
+
+val VetclinicAppRobotoFamily = FontFamily(
+    Font(R.font.roboto_medium, FontWeight.Medium),
+    Font(R.font.roboto_regular, FontWeight.Normal)
+)
+
+private val RegularWorkSansStyle = TextStyle(
+    fontFamily = VetclinicAppWorkSansFamily,
+    fontWeight = FontWeight.Normal,
+    platformStyle = PlatformTextStyle(
+        includeFontPadding = false
+    )
+)
+
+private val MediumWorkSansStyle = TextStyle(
+    fontFamily = VetclinicAppWorkSansFamily,
+    fontWeight = FontWeight.Medium,
+    platformStyle = PlatformTextStyle(
+        includeFontPadding = false
+    )
+)
+
+private val RegularRobotoStyle = TextStyle(
+    fontFamily = VetclinicAppRobotoFamily,
+    fontWeight = FontWeight.Normal,
+    platformStyle = PlatformTextStyle(
+        includeFontPadding = false
+    )
+)
+
+private val MediumRobotoStyle = TextStyle(
+    fontFamily = VetclinicAppRobotoFamily,
+    fontWeight = FontWeight.Medium,
+    platformStyle = PlatformTextStyle(
+        includeFontPadding = false
+    )
+)
+
+val RegularWorkSans24 = RegularWorkSansStyle.copy(
+    fontSize = 24.sp,
+    lineHeight = 28.sp
+)
+
+val MediumWorkSans20 = RegularWorkSansStyle.copy(
+    fontSize = 20.sp,
+    lineHeight = 28.sp
+)
+val MediumWorkSans16 = MediumWorkSansStyle.copy(
+    fontSize = 16.sp,
+    lineHeight = 28.sp
+)
+
+val RegularRoboto32 = RegularRobotoStyle.copy(
+    fontSize = 32.sp,
+    lineHeight = 40.sp
+)
+val RegularRoboto24 = RegularRobotoStyle.copy(
+    fontSize = 24.sp,
+    lineHeight = 32.sp
+)
+val RegularRoboto16 = RegularRobotoStyle.copy(
+    fontSize = 16.sp,
+    lineHeight = 24.sp
+)
+val RegularRoboto14 = RegularRobotoStyle.copy(
+    fontSize = 14.sp,
+    lineHeight = 20.sp
+)
+
+val MediumRoboto16 = MediumRobotoStyle.copy(
+    fontSize = 16.sp,
+    lineHeight = 24.sp
+)
+val MediumRoboto14 = MediumRobotoStyle.copy(
+    fontSize = 14.sp,
+    lineHeight = 20.sp
+)
+val MediumRoboto12 = MediumRobotoStyle.copy(
+    fontSize = 12.sp,
+    lineHeight = 16.sp
+)
